d2badd6c7c066a02d651c232b8f076cc09e4d081
user <user@192.168.154.1>
2020-04-13 08:15:25 +0700

    this is new project

---
+++ src/doan_tranlengochuyen_qlkh_test/DoAn_TranLeNgocHuyen_QLKH_Test.java
@@ -0,0 +1,12 @@
+
+package doan_tranlengochuyen_qlkh_test;
+
+
+public class DoAn_TranLeNgocHuyen_QLKH_Test 
+{
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
